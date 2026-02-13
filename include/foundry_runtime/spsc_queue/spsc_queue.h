a5c37885f58f7d9b8bb3bd1bda86b4edf60d7a57
Daniel Gunther <132302961+dgunther2001@users.noreply.github.com>
2026-02-12 22:16:48 -0500

    cached read and write locations to avoid acquiring atomics when not necessary

--- include/foundry_runtime/spsc_queue/spsc_queue.h
+++ include/foundry_runtime/spsc_queue/spsc_queue.h
@@ -29,6 +29,9 @@ template <class T, size_t capacity, bool enable_cacheline_padding, bool enable_p
 class spsc_queue {
     static_assert(capacity >= 2);
     static_assert(std::is_trivially_copyable_v<T>, "Trivially Copyable T NOT Provided...");    
+    static_assert((capacity & (capacity - 1)) == 0, "capacity must be power of two...");
+
+    static constexpr std::size_t capacity_mask = capacity - 1;
 
     struct alignas(cacheline_size) PaddedLine {
         std::atomic<std::size_t> r_w_index{0};
@@ -55,10 +58,24 @@ public:
     ~spsc_queue() = default;
 
     bool try_enqueue(const T& in_data) {
+
+        /*
+        Steps:
+            1. read the current index of the last write (atomic) => current_write_loc
+            2. increment the write pointer => next_loc
+            3. We now check if the location we want to write to is equal to what the read index was the last time this thread acquired it => (next_loc == cached_read_loc)
+                a. if they are equal, then we must check if we are trying to write to the current read index, so we acquire the atomic read index from the other thread and check if the write is valid
+                b. if the cached read index is not equal to our nexgt index, then we are safe to just write and don't acquire the read index atomic
+            4. We do a prefetch if it is enabled
+            5. We the queue write, increment the atomic write index, and release it
+        */
         auto current_write_loc = write_next.r_w_index.load(std::memory_order_relaxed);
         auto next_loc          = increment(current_write_loc);
 
-        if (next_loc == read_next.r_w_index.load(std::memory_order_acquire)) return false;
+        if (next_loc == cached_read_loc) {
+            cached_read_loc = read_next.r_w_index.load(std::memory_order_acquire);
+            if (next_loc == cached_read_loc) return false;
+        }
 
         if constexpr (enable_prefetch) sw_prefetch_write(&queue[current_write_loc]);
         queue[current_write_loc] = in_data;
@@ -71,7 +88,10 @@ public:
     bool try_dequeue(T& out_data) {
         auto current_read_loc = read_next.r_w_index.load(std::memory_order_relaxed);
 
-        if (current_read_loc == write_next.r_w_index.load(std::memory_order_acquire)) return false;
+        if (current_read_loc == cached_write_loc) {
+            cached_write_loc = write_next.r_w_index.load(std::memory_order_acquire);
+            if (current_read_loc == cached_write_loc) return false;
+        }
 
         if constexpr (enable_prefetch) sw_prefetch_read(&queue[current_read_loc]);
         out_data = queue[current_read_loc];
@@ -82,15 +102,15 @@ public:
     }
 
 private:
-    static constexpr std::size_t increment(std::size_t i) noexcept {
-        return (i + 1 == capacity) ? 0 : (i + 1);
-    }
-
+    static constexpr std::size_t increment(std::size_t i) noexcept { return (i + 1) & capacity_mask; }
 
     IndexType write_next{}; 
     IndexType read_next{}; 
 
-    T queue[capacity];
+    alignas(cacheline_size) std::size_t cached_read_loc = 0;
+    alignas(cacheline_size) std::size_t cached_write_loc = 0; 
+
+    alignas(cacheline_size) T queue[capacity];
 };
 
 };
